f29604ae31f51bf6f66a6d92cff168b1653fe018
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-09-13 17:47:03 -0300

    Implementing home page

---
+++ app/(root)/(home)/_components/cards.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import Image from "next/image";
+
+export const Cards = () => {
+  const [meeting, setMeeting] = useState<
+    "isSchedule" | "isJoining" | "isInstant" | undefined
+  >();
+
+  const router = useRouter();
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <div
+        className="bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full min-h-[260px] rounded-[14px] cursor-pointer"
+        onClick={() => setMeeting("isInstant")}
+      >
+        <div className="flex items-center justify-center glassmorphism size-12 rounded-[10px]">
+          <Image
+            src="/icons/add-meeting.svg"
+            height={27}
+            width={27}
+            alt="Add meeting"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">New Meeting</h1>
+          <p className="text-lg font-normal">Start an instant meeting</p>
+        </div>
+      </div>
+      <div
+        className="bg-[#0E78F9] px-4 py-6 flex flex-col justify-between w-full min-h-[260px] rounded-[14px] cursor-pointer"
+        onClick={() => setMeeting("isJoining")}
+      >
+        <div className="flex items-center justify-center glassmorphism size-12 rounded-[10px]">
+          <Image
+            src="/icons/join-meeting.svg"
+            height={27}
+            width={27}
+            alt="Join meeting"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">Join Meeting</h1>
+          <p className="text-lg font-normal">Via invitation link</p>
+        </div>
+      </div>
+      <div
+        className="bg-[#830EF9] px-4 py-6 flex flex-col justify-between w-full min-h-[260px] rounded-[14px] cursor-pointer"
+        onClick={() => setMeeting("isSchedule")}
+      >
+        <div className="flex items-center justify-center glassmorphism size-12 rounded-[10px]">
+          <Image
+            src="/icons/schedule.svg"
+            height={27}
+            width={27}
+            alt="Schedule"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">Schedule Meeting</h1>
+          <p className="text-lg font-normal">Plan your meeting</p>
+        </div>
+      </div>
+      <div
+        className="bg-[#F9A90E] px-4 py-6 flex flex-col justify-between w-full min-h-[260px] rounded-[14px] cursor-pointer"
+        onClick={() => router.push("/recordings")}
+      >
+        <div className="flex items-center justify-center glassmorphism size-12 rounded-[10px]">
+          <Image
+            src="/icons/recordings.svg"
+            height={27}
+            width={27}
+            alt="Recordings"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">View Recordings</h1>
+          <p className="text-lg font-normal">Meeting recordings</p>
+        </div>
+      </div>
+    </section>
+  );
+};
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,37 @@
+import { Cards } from "./_components/cards";
+
 const HomePage = () => {
-  return null;
+  const now = new Date();
+
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
+
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div
+        className="h-[300px] w-full rounded-[20px] bg-cover"
+        style={{
+          backgroundImage: "url('/images/hero-background.png')",
+        }}
+      >
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-[#C9DDFF]">{date}</p>
+          </div>
+        </div>
+      </div>
+      <Cards />
+    </section>
+  );
 };
 
 export default HomePage;
